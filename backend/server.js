3e4cea23cf82528ad8c8fbeae0c46873bee27a46
Hong-Layeang <layeang.hong@student.cadt.edu.kh>
2026-02-21 13:49:11 +0700

    fix import errors and database connection

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import authRoutes from '../src/routes/auth.routes.js';
-import categoryRoutes from '../src/routes/category.routes.js';
-import conversationRoutes from '../src/routes/conversation.routes.js';
-import messageRoutes from '../src/routes/message.routes.js';
-import productRoutes from '../src/routes/product.routes.js';
-import universityRoutes from '../src/routes/university.routes.js';
-import userRoutes from '../src/routes/user.routes.js';
-import connectDB, { testConnection } from '../src/config/database.js';
-import '../src/models/index.js';
+import authRoutes from './src/routes/auth.routes.js';
+import categoryRoutes from './src/routes/category.routes.js';
+import conversationRoutes from './src/routes/conversation.routes.js';
+import messageRoutes from './src/routes/message.routes.js';
+import productRoutes from './src/routes/product.routes.js';
+import universityRoutes from './src/routes/university.routes.js';
+import userRoutes from './src/routes/user.routes.js';
+import connectDB, { testConnection } from './src/config/database.js';
+import db from './src/models/index.js';
 
 dotenv.config();
 
@@ -41,9 +41,8 @@ const startServer = async () => {
         }
         console.log('Database connected');
         // Sync models (drops and recreates all tables in development)
-        await models.sequelize.sync({ force: true });
+        await db.sequelize.sync({ force: true });
         console.log('Tables synced');
-
 
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
